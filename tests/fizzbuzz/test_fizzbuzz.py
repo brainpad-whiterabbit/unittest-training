911c08e370ae641b04ac478a512d4fef2a77fe7b
harutoiwasaki <haruto.iwasaki@brainpad.co.jp>
2026-06-02 04:36:26 +0000

    tekito

--- tests/fizzbuzz/test_fizzbuzz.py
+++ tests/fizzbuzz/test_fizzbuzz.py
@@ -1,6 +1,8 @@
 from fizzbuzz.fizzbuzz import fizzbuzz
+import pytest
 
 
+# 3の倍数に対するテスト
 def test_fizzbuzz_example():
     # Arrange
     input_value = 3
@@ -11,3 +13,60 @@ def test_fizzbuzz_example():
 
     # Assert
     assert result == expected_output
+
+
+# 5の倍数に対するテスト
+def test_fizzbuzz_5multiple():
+    # Arrange
+    input_value = 5
+    expected_output = "Buzz"
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+# 3の倍数でも5の倍数ではない場合のテスト
+def test_fizzbuzz_notmultiple():
+    # Arrange
+    input_value = 1
+    expected_output = str(input_value)
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+# 15の倍数に対するテスト
+def test_fizzbuzz_15multiple():
+    # Arrange
+    input_value = 15
+    expected_output = "FizzBuzz"
+
+    # Act
+    result = fizzbuzz(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+# 範囲外に対するテスト
+def test_fizzbuzz_outrange():
+    with pytest.raises(ValueError):
+        fizzbuzz(1001)
+
+
+# 小数を入力したことに対するテスト
+def test_fizzbuzz_typeerror():
+    with pytest.raises(TypeError):
+        fizzbuzz(111.1)
+
+
+# 数値以外を入力したことに対するテスト
+def test_fizzbuzz_typeerror2():
+    with pytest.raises(TypeError):
+        fizzbuzz("aiueo")
